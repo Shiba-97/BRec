6e7cde66a3abc94387993cd0621b8792a8655918
Shiba-97 <114337100+Shiba-97@users.noreply.github.com>
2023-11-25 19:17:25 -0500

    ku

--- app.py
+++ app.py
@@ -15,3 +15,29 @@ def recommend(book):
   for i in booklist:
     var.append(final.iloc[i[0]].title)
   return var
+
+
+st.title('Books Recommender System')
+selected_book_name = st.selectbox(
+    'Select Books',
+    books['title'].values)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_book_name)
+    col1, col2, col3, col4, col5= st.columns(5)
+
+    with col1:
+        st.text(recommendations[0])
+        
+    with col2:
+        st.text(recommendations[1])
+        
+
+    with col3:
+        st.text(recommendations[2])
+        
+    with col4:
+       st.text(recommendations[3])
+    with col5:
+        st.text(recommendations[4])
+        
