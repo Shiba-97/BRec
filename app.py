779473fa371ad45c5ee5efa57097fa4075a8d75f
Shiba-97 <114337100+Shiba-97@users.noreply.github.com>
2023-11-26 18:05:04 -0500

    edits

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+
+# Load the saved objects from the binary files
 books_dict = pickle.load(open(r"books_dict.pkl",'rb'))
 sim = pickle.load(open(r"similarity.pkl",'rb'))
 books = pickle.load(open(r"books.pkl",'rb'))
 
 
+# Recommendation function
 def recommend(book):
   index=books[books['title']==str(book)].index[0]
   ss=sim[index]
@@ -16,36 +19,17 @@ def recommend(book):
     var.append(books.iloc[i[0]].title)
   return var
 
-
+# Streamlit web app
 st.title('Books Recommender System')
+
+# Dropdown for selecting books
 selected_book_name = st.selectbox(
     'Select Books',
     books['title'].values)
 
-
-
-
+# Button to trigger recommendations
 if st.button('Recommend'):
     recommendations = recommend(selected_book_name)
     for item in recommendations:
        st.subheader(item)
 
-# if st.button('Recommend'):
-#     recommendations = recommend(selected_book_name)
-#     col1, col2, col3, col4, col5= st.columns(5)
-
-#     with col1:
-#         st.text(recommendations[0])
-        
-#     with col2:
-#         st.text(recommendations[1])
-        
-
-#     with col3:
-#         st.text(recommendations[2])
-        
-#     with col4:
-#        st.text(recommendations[3])
-#     with col5:
-#         st.text(recommendations[4])
-        
